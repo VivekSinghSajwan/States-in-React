c9a40876d5e3c3d3ec677d3c30b5507b960440cd
VivekSinghSajwan <vssajwan09123@gmail.com>
2025-04-20 10:29:14 +0530

    show how re-rendering works

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,14 +3,22 @@ import {useState} from "react";
 function Counter(){
     const [count,setCount] = useState(0);
 
+    console.log("✅ Component rendered Outside");
+
     function inCount(){
         setCount(count+1);
     }
 
     return (
         <div>
-            <h1>States in React</h1>
-            <button onClick={inCount}>Count: {count}</button>
+            <h1>
+                {console.log("✅ Component rendered inside H1")}
+                States in React
+            </h1>
+            <button onClick={inCount}>
+                {console.log("✅ Component rendered inside btn")}
+                Count: {count}
+            </button>
         </div>
     )
 }
